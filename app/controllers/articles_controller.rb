89ebd6ce80560576c22f8c40ef7dc7f90adc81de
shrikant shinde <shrikant.shinde@coupa.com>
2020-04-03 11:41:35 +0530

    Display success and error message details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,23 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
